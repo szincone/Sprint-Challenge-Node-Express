777e153cc20627f5814b80075c3c1c0c39b335d3
szincone <sawyerzincone@gmail.com>
2018-08-31 10:08:58 -0600

    GET requests done for project data

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const server = express();
 // databases
-const projectDB = require("./data/helpers/actionModel.js");
-const actionDB = require("./data/helpers/projectModel.js");
+const actionDB = require("./data/helpers/actionModel.js");
+const projectDB = require("./data/helpers/projectModel.js");
 
 server.use(express.json());
 
+//////////////================================= START PROJECTSDB REQUESTS =================================//////////////
 // GET REQUEST //
 server.get("/projects", (req, res) => {
   projectDB
@@ -40,6 +41,29 @@ server.get("/projects/:id", (req, res) => {
     });
 });
 
-//////////////================================= END POSTDB REQUESTS =================================//////////////
+// GET USER POSTS REQUEST
+server.get("/projects/:id/actions", (req, res) => {
+  const { id } = req.params;
+  projectDB
+    .getProjectActions(id)
+    .then(actions => {
+      if (actions.length === 0) {
+        res.status(404).json({
+          message: "The project with the specified ID does not exist.",
+        });
+      } else {
+        return res.status(200).json({ actions });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+// end GETS //
+
+//////////////================================= END PROJECTSDB REQUESTS =================================//////////////
 
 server.listen(7000, () => console.log("\n== API on port 7k ==\n"));
