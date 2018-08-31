933723a6ae16aa23579cf30b766492c027834ec1
szincone <sawyerzincone@gmail.com>
2018-08-31 11:56:07 -0600

    added cors to so react-app could interact with the API

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const cors = require("cors");
 const projectRoutes = require("../projects/projectRoutes.js");
 const actionRoutes = require("../actions/actionRoutes.js");
 
 module.exports = server => {
   server.use(express.json());
+  server.use(cors());
   server.use("/projects", projectRoutes);
   server.use("/actions", actionRoutes);
 };
